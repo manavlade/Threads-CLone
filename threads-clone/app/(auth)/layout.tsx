a21552b2f82580a126eec436920267f2428a3995
manavlade <manavlade14690@gmail.com>
2024-04-09 16:58:39 +0530

    Defined folder structure for authentication

---
+++ threads-clone/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import React from "react"
+
+import '../global.css';
+
+export const metadata = {
+    title: 'Threads',
+    description: 'A Meta Threads Clone using Next.js 14'
+}
+
+const inter = Inter({
+    subsets: ["latin"]
+})
+
+export default function RootLayout ({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider >
+            <html lang="eng">
+                <body className={`${inter.className} bg-dark-1`} >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
